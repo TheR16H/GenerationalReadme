d46757a147892804fe700753c142d58fd9ada1c1
TheR16H <rashawn5hall@gmail.com>
2024-06-10 10:49:49 -0400

    filled out more codes

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,46 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = [
+    { description: "describe what will be in this respiratory" },
+    { installation: "are there any special instructions to install this?" },
+    { usage: "how is the content in this repo meant to be used?" },
+    { contributions: "what are the contribution guidelines?" },
+    { tests: "how should tests be performed" },
+    { username: "what is your github username?" },
+    { email: "what is your email?" }
+];
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    `## Description
+    ${description}
+
+    ## installation
+    ${installation}
+
+    ## Usage
+    ${usage}
+
+    ## contributions
+    ${contributions}
+
+    ## tests
+    ${tests};`
+}
+
+function writeToFile(fileName, data) {
+    return `# ${fileName}\n${data}`;
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions) 
+    .then((answers) => writeToFile('README.md', generateREADME(answers)))
+    .then(() => console.log('read the readmee'))
+    .catch((err) => console.error(err));
+}
 
 // Function call to initialize app
 init();
+
+
